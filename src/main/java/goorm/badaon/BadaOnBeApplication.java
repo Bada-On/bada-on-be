39991ffbf3d79b1bf3dac81ce335a00bc7fcf445
Hyun <itsmine096@gmail.com>
2024-10-31 09:23:46 +0900

    [init] 프로젝트 생성

---
+++ src/main/java/goorm/badaon/BadaOnBeApplication.java
@@ -0,0 +1,13 @@
+package goorm.badaon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadaOnBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadaOnBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/goorm/badaon/BadaOnBeApplicationTests.java
@@ -0,0 +1,13 @@
+package goorm.badaon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadaOnBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
